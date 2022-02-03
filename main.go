6846b4708b6c19fb1aa687049c0b6b1f07f74a8d
Matt Lewis <matt@mplewis.com>
2022-02-02 22:58:12 -0700

    wip: basic router

--- main.go
+++ main.go
@@ -8,15 +8,94 @@ import (
 	"encoding/base64"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/a-h/gemini"
-	"github.com/a-h/gemini/mux"
 )
 
 type User struct {
 	Certificate     *x509.Certificate
 	CommonName      string
 	CertificateHash string
+}
+
+type Handler struct {
+	routes []Route
+}
+
+type RequestParams struct {
+	Path string
+	User *User
+}
+
+type HandlerMatch struct {
+	Handler Handler
+	Params  map[string]string
+}
+
+type RoutePart struct {
+	Key     string
+	Matcher bool
+}
+
+type Route struct {
+	parts []RoutePart
+}
+
+func NewHandler(routes ...Route) Handler {
+	return Handler{routes}
+}
+
+func NewRoute(path string) Route {
+	raws := strings.Split(strings.Trim(path, "/"), "/")
+	parts := []RoutePart{}
+	for _, raw := range raws {
+		if strings.HasPrefix(raw, ":") {
+			parts = append(parts, RoutePart{
+				Key:     raw[1:],
+				Matcher: true,
+			})
+		} else {
+			parts = append(parts, RoutePart{
+				Key:     raw,
+				Matcher: false,
+			})
+		}
+	}
+	return Route{parts}
+}
+
+func (r Route) Match(path string) (map[string]string, bool) {
+	raws := strings.Split(strings.Trim(path, "/"), "/")
+	if len(raws) != len(r.parts) {
+		return nil, false
+	}
+	params := map[string]string{}
+	for i, a := range raws {
+		b := r.parts[i]
+		if b.Matcher {
+			params[b.Key] = a
+		} else if a != b.Key {
+			return nil, false
+		}
+	}
+	return params, true
+}
+
+func (h Handler) ServeGemini(w gemini.ResponseWriter, r *gemini.Request) {
+	log.Println(r.URL.Path)
+	log.Println(h.routes)
+	for _, route := range h.routes {
+		params, ok := route.Match(r.URL.Path)
+		if !ok {
+			continue
+		}
+		log.Println(params)
+		w.Write([]byte("hello"))
+		return
+	}
+
+	w.SetHeader(gemini.CodeNotFound, "not found")
 }
 
 func hash(data string) string {
@@ -42,28 +121,21 @@ func getUser(c gemini.Certificate) (*User, error) {
 }
 
 func main() {
-	router := mux.NewMux()
-	router.AddRoute("/", gemini.HandlerFunc(func(w gemini.ResponseWriter, r *gemini.Request) {
-		user, err := getUser(r.Certificate)
-		if err != nil {
-			log.Panic(err)
-		}
-
-		if user == nil {
-			w.Write([]byte("Not logged in"))
-			return
-		}
-
-		msg := fmt.Sprintf("%s => %s", user.CommonName, user.CertificateHash)
-		w.Write([]byte(msg))
-	}))
-
 	cert, err := tls.LoadX509KeyPair("localhost.crt", "localhost.key")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	domain := gemini.NewDomainHandler("localhost", cert, router)
+	h := NewHandler(
+		NewRoute("/"),
+		NewRoute("/foo/:bar"),
+	)
+
+	r := NewRoute("/foo/:bar")
+	fmt.Println(r)
+	fmt.Println(r.Match("/foo/quux"))
+
+	domain := gemini.NewDomainHandler("localhost", cert, h)
 	err = gemini.ListenAndServe(context.Background(), ":1965", domain)
 	if err != nil {
 		log.Fatal("error:", err)
